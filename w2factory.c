b5a7672cd0b3ba2dfae760995f8a44bc737713eb
Milad Mohtashamirad <milad.mohtashamirad@morsemicro.com>
2022-11-15 11:59:31 +1100

    better protection.

--- w2factory.c
+++ w2factory.c
@@ -1,11 +1,42 @@
-#include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mtd/mtd.h>
+#include <linux/err.h>
+
+/*
+ * This module is intended for embedded devices where the mtd partition
+ * layout may be hard-coded in the firmware. If, for some reason, you
+ * DO have to write to a read-only partition (which is often a bad idea),
+ * this module is the way to go.
+ *
+ * The module is currently limited to the first 64 partitions, but this
+ * should suffice for most purposes.
+ *
+ * Inspired by dougg3@electronics.stackexchange.com:
+ * https://electronics.stackexchange.com/a/116133/97342
+ */
+
+#ifndef MODULE
+#error "Must be compiled as a module."
+#endif
+
+#define MOD_WARNING KERN_WARNING "mtd-rw: "
+#define MOD_INFO KERN_INFO "mtd-rw: "
+#define MOD_ERR KERN_ERR "mtd-rw: "
+
+static bool i_want_a_brick = false;
+module_param(i_want_a_brick, bool, S_IRUGO);
+MODULE_PARM_DESC(i_want_a_brick, "Make all partitions writeable");
 
 int w2factory_init(void)
 {
     struct mtd_info *mtd;
-    
+    if (!i_want_a_brick)
+    {
+        printk(MOD_ERR "must specify i_want_a_brick=1 to continue\n");
+        return -EINVAL;
+    }
+
     mtd = get_mtd_device_nm("factory");
     printk(KERN_INFO "value before, 0x%x\n",mtd->flags);
     mtd->flags |= MTD_WRITEABLE;
